236951d0c38539e79b953258b6c5ed43db4d6bbb
Laura <laurasarabiaf@gmail.com>
2021-02-08 13:37:49 -0300

    mostrar citas

---
+++ routes/principal.js
@@ -0,0 +1,43 @@
+const { Router } = require('express');
+const router = Router();
+const quotes = 
+[
+  {
+      author: "Anonimus",
+      text: "Hay que fallar antes de que funcione"
+  },
+  {
+    author: "Bob Marley",
+    text: "El dinero no puede comprar la vida"
+  },
+  {
+    author: "René Descartes",
+    text: "Pienso, luego existo"
+  },   
+  {
+    author: "Mahatma Gandhi",
+    text: "Ojo por ojo y el mundo acabará ciego"
+  }     
+]
+
+router.get('/', function(req, res) {
+  res.render('index');
+});
+
+router.get('/quotes', function(req, res) {
+  res.render('quotes', {quotes:quotes});
+});
+
+router.post("/quotes", (req, res) => {
+  res.render('quotes', {quotes:quotes});
+});
+
+router.post("/quotes", (req, res) => {
+  const newquote = req.body
+  console.log(newquote);
+  quotes.push(req.body.author);
+  quotes.push(req.body.text);
+  //quotes.push(newquote)
+  res.render("quotes", {quotes:quotes})  
+});
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const port = 8000;
+
+app.use('/static', express.static('static'))
+//app.use(express.static(__dirname + "/static"));
+app.set('views', __dirname + '/views'); 
+app.set('view engine', 'ejs');
+app.use( express.json() );
+
+app.use(require('./routes/principal'));
+
+app.listen(8000, function() {
+  console.log("listening on port 8000");
+});
